0569e34abf5adc35d14c270ed32fd5b0009db662
Emotional_bug <1541373309@qq.com>
2024-08-03 23:09:57 +0800

    study

---
+++ deeper/sort.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 郝建斌 on 2024/8/3.
+// c++ sort
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+//int main(){
+//    std::vector<int> values = {3,5,1,4,2};
+//    std::sort(values.begin(),values.end()); // 升序
+//    std::sort(values.begin(),values.end(),std::greater<int>()); // 降序
+//    std::sort(values.begin(),values.end(),[](int a,int b){return a > b;}); // 自定义比较函数
+//    for (int value: values) {
+//        std::cout << value << std::endl;
+//    }
+//}
--- deeper/thread.cpp
+++ deeper/thread.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <thread>
+#include <chrono>
 
 void DoWork(){
     using namespace std::chrono_literals;
@@ -12,9 +13,12 @@ void DoWork(){
     std::this_thread::sleep_for(1s);
 }
 
-int main(){
-    std::thread worker(DoWork);
-    worker.join();
-
-    std::cin.get();
-}
+//int main(){
+//    auto start = std::chrono::high_resolution_clock::now();
+//    std::thread worker(DoWork);
+//    worker.join();
+//    auto end = std::chrono::high_resolution_clock::now();
+//    std::chrono::duration<float> duration = end - start;
+//    std::cout << duration.count() << "ms" << std::endl;
+//    std::cin.get();
+//}
